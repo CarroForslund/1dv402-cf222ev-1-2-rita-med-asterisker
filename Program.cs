efed3c4299d13ab43d3e1cc46eb4b63dd5c6c362
unknown <cf222ev@NY108X02.lnu.se>
2014-09-10 15:32:55 +0200

    the first step

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_rita_med_asterisker_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++) // rader
+            {
+                for (int j = 0; j < 39; j++) // kolumner
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                if (i % 2 == 0) // för jämna tal
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
