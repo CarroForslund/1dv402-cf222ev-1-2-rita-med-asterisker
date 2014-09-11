b9ed1728744bb72485161091ac30d4a81bc99184
unknown <cf222ev@Ny106x01.lnu.se>
2014-09-11 10:07:21 +0200

    done

--- Program.cs
+++ Program.cs
@@ -10,14 +10,30 @@ namespace _1_2_rita_med_asterisker_a
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 25; i++) // rader
+            // Beräkna och skriv ut mönster
+            // 39 kolumner och 25 rader
+            // Varannan rad börjar med ett indrag
+            // Var tredje rad är grön, magenta respektive gul
+            for (int rowNumber = 0; rowNumber < 25; rowNumber++) 
             {
-                for (int j = 0; j < 39; j++) // kolumner
+                for (int columnNumber = 0; columnNumber < 39; columnNumber++)
                 {
+                    switch (rowNumber % 3)
+                    {
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        default :
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                    }
                     Console.Write("* ");
                 }
                 Console.WriteLine();
-                if (i % 2 == 0) // för jämna tal
+                if (rowNumber % 2 == 0) // för jämna tal
                 {
                     Console.Write(" ");
                 }
